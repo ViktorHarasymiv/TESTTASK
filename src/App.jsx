fcf519c840cc4cf41f4bebd8efd5863207492c5b
Viktor Harasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-07-28 13:34:49 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./UserContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./components/App/App.jsx";
+
+import "./main.scss";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
