496efba6ca0895881e348b1250c32933d6498bb3
Viktor Harasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-07-28 15:28:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./components/App/App.jsx";
 
-// import "./main.scss";
+import "./main.scss";
+
+eruda.init();
 
 const queryClient = new QueryClient();
 
