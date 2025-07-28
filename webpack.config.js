49dccb7fc133370137ebf5e8d82504fc0d7cc199
Viktor Harasymiv <115593029+ViktorHarasymiv@users.noreply.github.com>
2025-07-28 14:35:41 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
+  devtool: "source-map",
   entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, "dist"),
