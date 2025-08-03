df190f0e42882577ac62a35e17328261e9271160
Viktor <victorharasymiv97@gmail.com>
2025-08-03 18:01:27 +0200

    New update 18:01

--- src/components/layout/Contacts/ContactCard.jsx
+++ src/components/layout/Contacts/ContactCard.jsx
@@ -15,41 +15,43 @@ export default function ContactCard({ user }) {
   const [isHoverEmail, setIshoverEmail] = useState(false);
 
   return (
-    <li className="user__tile">
-      <figure className="user__avatar">
-        <img
-          src={photo ? photo : "/images/icons/photo-cover.png"}
-          alt={`Фото:${name}`}
-          width={70}
-          height={70}
-          loading="lazy"
-        />
-      </figure>
-      <div className="user__name-wrapper">
-        <h2
-          onMouseEnter={() => setIshover(true)}
-          onMouseLeave={() => setIshover(false)}
-          className="user__name"
-        >
-          {name}
-        </h2>
-        {isHover && <HoverTextPreview name={name} />}
-      </div>
-      <div className="user__contact-wrapper">
-        <h3 className="user__position">{position}</h3>
-        <div className="user__email-wrapper">
-          <a
-            href={`mailto:${email}`}
-            className="user__email"
-            onMouseEnter={() => setIshoverEmail(true)}
-            onMouseLeave={() => setIshoverEmail(false)}
+    <li>
+      <article className="user__tile">
+        <figure className="user__avatar">
+          <img
+            src={photo ? photo : "/images/icons/photo-cover.png"}
+            alt={`Фото:${name}`}
+            width={70}
+            height={70}
+            loading="lazy"
+          />
+        </figure>
+        <div className="user__name-wrapper">
+          <h2
+            onMouseEnter={() => setIshover(true)}
+            onMouseLeave={() => setIshover(false)}
+            className="user__name"
           >
-            {email}
-          </a>
-          {isHoverEmail && <HoverTextPreview name={email} />}
+            {name}
+          </h2>
+          {isHover && <HoverTextPreview name={name} />}
         </div>
-        <a href={`tel:${phone}`}>{phone}</a>
-      </div>
+        <div className="user__contact-wrapper">
+          <h3 className="user__position">{position}</h3>
+          <div className="user__email-wrapper">
+            <a
+              href={`mailto:${email}`}
+              className="user__email"
+              onMouseEnter={() => setIshoverEmail(true)}
+              onMouseLeave={() => setIshoverEmail(false)}
+            >
+              {email}
+            </a>
+            {isHoverEmail && <HoverTextPreview name={email} />}
+          </div>
+          <a href={`tel:${phone}`}>{phone}</a>
+        </div>
+      </article>
     </li>
   );
 }
--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 
-import { useState, useEffect } from "react";
-
 export default function Footer() {
-  const [seconds, setSeconds] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    let interval;
-
-    if (isRunning) {
-      interval = setInterval(() => {
-        setSeconds((prev) => prev + 1);
-      }, 1000);
-    }
-
-    // Очищення при зупинці або розмонтуванні
-    return () => clearInterval(interval);
-  }, [isRunning]);
-
   return (
     <footer className="footer">
       <div className="footer__wrapper container">
-        <div>
-          <h2>Секунди: {seconds}</h2>
-          <button onClick={() => setIsRunning(true)}>Старт</button>
-        </div>
         &copy; abz.agency specially for the test task
       </div>
     </footer>
